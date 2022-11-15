6e739cbbb19b5b9d763ffb1f20f381e3b7777935
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2022-11-15 09:16:19 +0700

    update models

--- models/Product.js
+++ models/Product.js
@@ -2,7 +2,22 @@ const mongoose = require("mongoose");
 
 const ProductSchema = new mongoose.Schema(
    {
-      title: {
+      username: {
+         type: String,
+         required: true,
+      },
+      name: {
+         type: String,
+         required: true,
+      },
+      full_name: {
+         type: String,
+         required: true,
+      },
+      photo: {
+         type: String,
+      },
+      price: {
          type: String,
          required: true,
       },
@@ -10,16 +25,15 @@ const ProductSchema = new mongoose.Schema(
          type: String,
          required: true,
       },
-      photo: {
-         type: String,
-      },
-      username: {
-         type: String,
-         required: true,
+      spec: {
+         type: Array,
       },
       categories: {
          type: Array,
          required: true,
+      },
+      variant: {
+         type: Array,
       },
    },
    { timestamps: true }
